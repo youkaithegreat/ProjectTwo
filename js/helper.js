b52dfc080149df4af1a7b04f100ea033a0e26da8
Kevin <yangkevint@gmail.com>
2016-01-30 00:55:38 +0900

    Added some comments.

--- js/helper.js
+++ js/helper.js
@@ -6,8 +6,6 @@
 
  Cameron Pittman
  */
-
-
 /*
  These are HTML strings. As part of the course, you'll be using JavaScript functions
  replace the %data% placeholder text you see in them.
@@ -64,10 +62,9 @@ var googleMap = '<div id="map"></div>';
 /*
  The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
  */
-$(document).ready(function () {
-    $('button').click(function () {
-        var iName = inName(bio.full) || function () {
-            };
+$(document).ready(function() {
+    $('button').click(function() {
+        var iName = inName(bio.full) || function() {};
         $('#name').html(iName);
     });
 });
@@ -88,16 +85,14 @@ function inName(name) {
 clickLocations = [];
 
 function logClicks(x, y) {
-    clickLocations.push(
-        {
-            x: x,
-            y: y
-        }
-    );
+    clickLocations.push({
+        x: x,
+        y: y
+    });
     console.log('x location: ' + x + '; y location: ' + y);
 }
 
-$(document).click(function (loc) {
+$(document).click(function(loc) {
     var x = loc.pageX;
     var y = loc.pageY;
     console.log(logClicks(x, y));
@@ -108,7 +103,7 @@ $(document).click(function (loc) {
  See the documentation below for more details.
  https://developers.google.com/maps/documentation/javascript/reference
  */
-var map;    // declares a global map variable
+var map; // declares a global map variable
 
 
 /*
@@ -145,7 +140,7 @@ function initializeMap() {
         // the locations array. Note that forEach is used for array iteration
         // as described in the Udacity FEND Style Guide:
         // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-        education.schools.forEach(function (school) {
+        education.schools.forEach(function(school) {
             locations.push(school.location);
         });
 
@@ -153,7 +148,7 @@ function initializeMap() {
         // the locations array. Note that forEach is used for array iteration
         // as described in the Udacity FEND Style Guide:
         // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-        work.jobs.forEach(function (job) {
+        work.jobs.forEach(function(job) {
             locations.push(job.location);
         });
 
@@ -168,10 +163,10 @@ function initializeMap() {
     function createMapMarker(placeData) {
 
         // The next lines save location data from the search result object to local variables
-        var lat = placeData.geometry.location.lat();  // latitude from the place service
-        var lon = placeData.geometry.location.lng();  // longitude from the place service
-        var name = placeData.formatted_address;   // name of the place from the place service
-        var bounds = window.mapBounds;            // current boundaries of the map window
+        var lat = placeData.geometry.location.lat(); // latitude from the place service
+        var lon = placeData.geometry.location.lng(); // longitude from the place service
+        var name = placeData.formatted_address; // name of the place from the place service
+        var bounds = window.mapBounds; // current boundaries of the map window
 
         // marker is an object with additional data about the pin for a single location
         var marker = new google.maps.Marker({
@@ -188,7 +183,7 @@ function initializeMap() {
         });
 
         // hmmmm, I wonder what this is about...
-        google.maps.event.addListener(marker, 'click', function () {
+        google.maps.event.addListener(marker, 'click', function() {
             // your code goes here!
         });
 
@@ -222,7 +217,7 @@ function initializeMap() {
         var service = new google.maps.places.PlacesService(map);
 
         // Iterates through the array of locations, creates a search object for each location
-        locations.forEach(function (place) {
+        locations.forEach(function(place) {
             // the search request object
             var request = {
                 query: place
@@ -255,7 +250,7 @@ window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-window.addEventListener('resize', function (e) {
+window.addEventListener('resize', function(e) {
     //Make sure the map bounds get updated on page resize
     map.fitBounds(mapBounds);
 });
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,10 @@
 /*
  Each JSON object has a function inside. All functions are called at the very bottom of this page
+
+ All JSON data is formatted first and then either prepended or appended to the index.html file with the new formatting from helper.js.
+ for-in loops loop through the JSON object arrays and do the same steps.
  */
-
-
+//created bio JSON object with information that will propagate the header
 var bio = {
     "full": "Kevin Yang",
     "role": "Front End Developer",
@@ -16,15 +18,15 @@ var bio = {
     "welcomeMessage": "Welcome to my interactive resume!",
     "skills": ["Javascript", "HTML", "CSS", "Java", "Android Studio", "XML", "Adobe Photoshop", "Adobe Illustrator", "Git", "Photography"],
     "biopic": "images/profilepic.jpg",
-    "display": function () {
-
+    "display": function() {
+        //function propagates the head with the information from bio JSON object
 
         var formattedrole = HTMLheaderRole.replace("%data%", bio.role);
         $("#header").prepend(formattedrole);
 
         var formattedFirst = HTMLheaderName.replace("%data%", bio.full);
         $("#header").prepend(formattedFirst);
-
+        //these two are in reverse prepend, so that they are above the contact items.
 
         var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
         $("#topContacts:last").append(formattedMobile);
@@ -54,6 +56,7 @@ var bio = {
         var formattedBiopic = HTMLbioPic.replace("%data%", bio.biopic);
         $("#header").append(formattedBiopic);
 
+        //check to see that skills exist
         if (bio.skills.length > 0) {
             $("#header").append(HTMLskillsStart);
         }
@@ -71,8 +74,8 @@ var bio = {
 };
 
 
-var education =
-{
+//education JSON object. Follows the same pattern listed above.
+var education = {
     "schools": [{
         "name": "The University of Texas at Austin",
         "location": "Austin",
@@ -81,32 +84,30 @@ var education =
         "dates": "2012",
         "url": "http://www.utexas.edu"
 
-        },
-        {
-            "name": "National Cheng Chi University",
-            "location": "Taipei, Taiwan",
-            "degree": "Study Abroad",
-            "majors": "N/A",
-            "dates": "2010",
-            "url": "http://www.nccu.edu"
+    }, {
+        "name": "National Cheng Chi University",
+        "location": "Taipei, Taiwan",
+        "degree": "Study Abroad",
+        "majors": "N/A",
+        "dates": "2010",
+        "url": "http://www.nccu.edu"
 
-        },
-        {
-            "name": "Sophia University",
-            "location": "Tokyo, Japan",
-            "degree": "Study Abroad",
-            "majors": "East Asian Studies",
-            "dates": "2011",
-            "url": "http://www.sophia.ac.jp/eng"
+    }, {
+        "name": "Sophia University",
+        "location": "Tokyo, Japan",
+        "degree": "Study Abroad",
+        "majors": "East Asian Studies",
+        "dates": "2011",
+        "url": "http://www.sophia.ac.jp/eng"
 
-        }],
+    }],
     "onlineCourses": [{
         "title": "Udacity Front End Development Nanodegree",
         "school": "Udacity",
         "date": "2015",
         "url": "http://www.udacity.com"
     }],
-    "display": function () {
+    "display": function() {
 
         for (var school in education.schools) {
             if (education.schools.hasOwnProperty(school)) {
@@ -129,6 +130,8 @@ var education =
 
             }
         }
+
+        //appended to education, so the placement is correct
         $("#education").append(HTMLonlineClasses);
 
 
@@ -151,9 +154,8 @@ var education =
     }
 };
 
-
-var work =
-{
+//work JSON object, follows the same pattern as above.
+var work = {
     "jobs": [{
         "employer": "Chungdahm Institute",
         "title": "Head Instructor",
@@ -166,16 +168,14 @@ var work =
         "location": "Houston, Texas",
         "dates": "temp. Contract 2014",
         "description": "Physical migration of desktop computers between locations."
-    },
-        {
-            "employer": "Gloria English School",
-            "title": "Foreign English Teacher",
-            "location": "Taipei, Taiwan",
-            "dates": "2012-2013",
-            "description": "Taught English to all levels."
-        }
-    ],
-    "display": function () {
+    }, {
+        "employer": "Gloria English School",
+        "title": "Foreign English Teacher",
+        "location": "Taipei, Taiwan",
+        "dates": "2012-2013",
+        "description": "Taught English to all levels."
+    }],
+    "display": function() {
         $("#workExperience").append(HTMLworkStart);
 
         for (var job in work.jobs) {
@@ -200,9 +200,8 @@ var work =
 
 };
 
-
-var projects =
-{
+//project JSON object, also follows the same general pattern.
+var projects = {
     "projects": [{
         "title": "Online Portfolio",
         "url": "http://www.kevintyang.com/projectone",
@@ -210,7 +209,7 @@ var projects =
         "description": "Personal Portfolio Website with a responsive layout. Built with Javascript, HTML, CSS, jQuery and Bootstrap",
         "images": ["images/projectone.png"]
     }],
-    "display": function () {
+    "display": function() {
         $("#projects").append(HTMLprojectStart);
 
         for (var project in projects.projects) {
@@ -250,5 +249,3 @@ education.display();
 //functions to append the map and name formatting button
 $("#mapDiv").append(googleMap);
 $("#main").append(internationalizeButton);
-
-
