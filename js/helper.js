8180c0b0e463b86dee04dbc7fddca0b262c6b961
Kevin <yangkevint@gmail.com>
2016-01-29 23:16:08 +0900

    Completed project before proofread and formatting.

--- js/helper.js
+++ js/helper.js
@@ -87,10 +87,10 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  console.log(logClicks(loc));
+  var x = loc.pageX;
+  var y = loc.pageY;
+  console.log(logClicks(x,y));
 });
-
-
 
 /*
 This is the fun part. Here's where we generate the custom Google Map for the website.
@@ -240,11 +240,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
